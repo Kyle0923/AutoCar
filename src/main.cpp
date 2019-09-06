018222685bcf0006fc6ee74d3d8346c559d46188
kyle <kyle0923@qq.com>
2019-09-06 13:10:04 -0400

    HelloWorld demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 
 int main(int argc, char *argv[])
 {
+    std::cout << "Hello World" << std::endl;
+    while (argc > 1)
+    {
+        std::cout << argv[--argc] << std::endl;
+    }
     return 0;
 }
